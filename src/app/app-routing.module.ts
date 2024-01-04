0affebf45c3724b8a14b801524df44ea2e58e13b
Jacek <jacunio122@gmail.com>
2024-01-04 15:55:27 +0100

    Folder structure + routing + pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MoviePageComponent } from './pages/movie-page/movie-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: PageNotFoundComponent
+  },
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'movie:id',
+    component: MoviePageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
